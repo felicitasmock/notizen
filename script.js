ea6ea897b79bcccaedb88d2b52f3ec153ca2dbc9
felicitasmock <81470249+felicitasmock@users.noreply.github.com>
2021-05-12 10:38:10 +0200

    added JS DOC comments

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-
-
 let notes = [];
 let trash = [];
 
+/**
+ * function to load json from server
+ */
 async function init() {
     setURL('http://felicitas-mock.developerakademie.com/smallest_backend_ever');
     await downloadFromServer();
     showNotes();
 }
 
+/**
+ * function to show loaded notes and trash notes from server
+ */
 function showNotes() {
     document.getElementById('myNotes').innerHTML = ''; //Liste myNotes soll gelöscht werden, da sonst alle bestehenden noch ein mal angezeigt werden
     //notes = JSON.parse(backend.getItem('notes')) || [];
@@ -28,7 +32,9 @@ function showNotes() {
     document.getElementById('note').value = ''; // Wert aus Eingabefeld rauslöschen
 }
 
-
+/**
+ * function to add a note
+ */
 function addNote() {
     let text = document.getElementById('note').value; // füge die Notizen aus dem Feld mit der id "note" der variablen "text" hinzu
     if (text == '') {
@@ -42,7 +48,9 @@ function addNote() {
     }
 }
 
-// show trash notes in trash
+/**
+ * function to show deleted (moved) notes in trash
+ */
 async function showTrash() {
     document.getElementById('myTrash').innerHTML = ''; //Liste myTrash soll gelöscht werden, da sonst alle bestehenden noch ein mal angezeigt werden
     trash = await getArray('trash');
@@ -51,26 +59,33 @@ async function showTrash() {
     } else {
         for (let i = 0; i < trash.length; i++) {
             myTrash.innerHTML += `
-        <div class="notes">
-        ${trash[i]}
-        <div class="notes-btn-container">
-        <button class="delete-btn" onclick="deleteNote(${i})">Löschen</button>
-        </div>
-        </div>`;
+                <div class="notes">
+                ${trash[i]}
+                <div class="notes-btn-container">
+                <button class="delete-btn" onclick="deleteNote(${i})">Löschen</button>
+                </div>
+                </div>
+            `;
         }
     }
-
 }
 
-// function delete note in trash array
+/**
+ * function to delete note in trash (forever)
+ * 
+ * @param {number} position - This is the variable of the deleted note
+ */
 function deleteNote(position) {
     trash.splice(position, 1);
     setArray('trash', trash);
-    // aktualisiert local storage, da sonst die notiz noch dort drinsteht udn durch shownotes auch weiterhin angezeigt wird
     showTrash();
 }
 
-// function to delete note and move note to trash
+/**
+ * function to "delete" note out of notes - and moves into trash 
+ * 
+ * @param {number} i - This is the variable of the deleted/moved note
+ */
 function moveDeletedNote(i) {
     let trashNote = notes[i] // defines variable for deleted note
     notes.splice(i, 1); // deletes note in notes 
@@ -79,10 +94,11 @@ function moveDeletedNote(i) {
     setArray('trash', trash);
     setArray('notes', notes); // aktualisiert local storage, da sonst die notiz noch dort drinsteht udn durch shownotes auch weiterhin angezeigt wird
     showNotes();
-
 }
 
-// opens trash
+/**
+ * function to open trash / navi
+ */
 function openTrash() {
     document.getElementById('myTrash').style.display = "flex"; // show trash
     document.getElementById('noteInput').style.display = "none"; // hides input field
@@ -93,7 +109,10 @@ function openTrash() {
     closeMobileMenu()
     showTrash();
 }
-// opens notes
+
+/**
+ * function to open notes / navi
+ */
 function openNotes() {
     document.getElementById('navTrash').classList.remove('navi-left-active');
     document.getElementById('navNotes').classList.add('navi-left-active');
@@ -105,29 +124,44 @@ function openNotes() {
     showNotes();
 }
 
-//open mobile Menu
+/**
+ * function to open mobile menu / navi
+ */
 function openMobileMenu() {
-    //document.getElementById('mobileMenu').classList.remove('hide'); //show container
     let menu = document.getElementById('menu').innerHTML; // define variable for menu / nav
+    
     // puts menu into mobileMenu
     document.getElementById('mobileMenu').innerHTML = ` 
-    <div class="mobile-menu">
-    <div class="close-mobile-menu" onclick="closeMobileMenu()">Schließen</div>
-    ${menu}
-    </div>
+        <div class="mobile-menu">
+        <div class="close-mobile-menu" onclick="closeMobileMenu()">Schließen</div>
+        ${menu}
+        </div>
     `;
 }
 
-// close mobile menu
+/**
+ * function to close mobile menu
+ */
 function closeMobileMenu() {
     document.getElementById('mobileMenu').innerHTML = '';
 }
 
-// sets arry in local storage
+/**
+ * function to sets JSON on server
+ * 
+ * @param {string} key - This is the variable for the key to set JSON on server
+ * @param {string} array - This is the variable for the array saved on server
+ */
 async function setArray(key, array) {
     await backend.setItem(key, JSON.stringify(array));
 }
 
+/**
+ * function to get saved JSON on server
+ * 
+ * @param {string} key - This is the variable for the key to get JSON from server
+ * @returns - returns saved JSON from Server
+ */
 function getArray(key) {
     return JSON.parse(backend.getItem(key)) || [];
     // gibt mir das was im local storage steht, ODER (||) gibt mir nichts ([])
